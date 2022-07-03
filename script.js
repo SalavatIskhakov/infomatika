d99c23602e7ccf80fe67f52ffd2815de2e0bca6e
NatGames <salavat-iskhakov@mail.ru>
2022-07-03 09:06:40 +0300

    hexagons_visibility

--- script.js
+++ script.js
@@ -22,40 +22,44 @@ const events = [
 ];
 
 const hexagons = document.querySelectorAll('.events li');
+hexagons[0].style.visibility = 'hidden';
+hexagons[6].style.visibility = 'hidden';
+
 let eventIndex = 2;
+
 initial();
 
 function initial() {
-  hexagons.forEach((hexagon, j) => {
-    hexagon.querySelector('.hexagon_text').innerHTML = events[j].title;
-    hexagon.style.visibility = events[j].visibility;
-  })
+  for (let i = 0; i < 5; i++) {
+    hexagons[i + 1].querySelector('.hexagon_text').innerHTML = events[i].title;
+  }
 }
 
 function scrollLeft() {
   if (eventIndex > 3) {
     return;
   }
+
   let buf = hexagons[0].className;
   for (let i = 0; hexagons[i + 1]; i++) {
     hexagons[i].className = hexagons[i + 1].className;
   }
   hexagons[hexagons.length - 1].className = buf;
-  eventIndex++;
-  console.log(hexagons, eventIndex)
 
+  eventIndex++;
 }
 
 function scrollRight() {
   if (eventIndex < 1) {
     return;
   }
+
   let buf = hexagons[hexagons.length - 1].className;
   for (let i = hexagons.length - 1; i > 0; i--) {
     hexagons[i].className = hexagons[i - 1].className;
   }
   hexagons[0].className = buf;
-  console.log(eventIndex + 2, eventIndex)
+
   eventIndex--;
 }
 
@@ -63,12 +67,14 @@ let flag = true;
 document.addEventListener('wheel', (e) => {
   if (flag) {
     flag = false;
+
     if (e.deltaY < -30) {
       scrollRight();
     }
     if (e.deltaY > 30) {
       scrollLeft();
     }
+
     setTimeout(() => flag = true, 300)
   }
 }, true);
