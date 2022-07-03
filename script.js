3d27f8e3840a39e27ed06827b1a77639ded0e66b
NatGames <salavat-iskhakov@mail.ru>
2022-07-03 22:59:16 +0300

    add data

--- script.js
+++ script.js
@@ -1,27 +1,59 @@
 const events = [
   {
-    title: '1',
-    visibility: 'visible',
+    place: 'Стадион 1',
+    day: '30 мая',
+    time: '19:00',
+    link: '#',
+    teams: [
+      'Соперник №1',
+      'Соперник №3',
+    ],
   },
   {
-    title: '2',
-    visibility: 'visible',
+    place: 'Стадион 2',
+    day: '17 июня',
+    time: '19:00',
+    link: '#',
+    teams: [
+      'Соперник №4',
+      'Соперник №5',
+    ],
   },
   {
-    title: '3',
-    visibility: 'visible',
+    place: 'Стадион 3',
+    day: '26 июня',
+    time: '19:00',
+    link: 'https://yandex.ru/',
+    teams: [
+      'Соперник №1',
+      'Соперник №2',
+    ],
   },
   {
-    title: '4',
-    visibility: 'visible',
+    place: 'Стадион 4',
+    day: '16 июля',
+    time: '19:00',
+    link: '#',
+    teams: [
+      'Соперник №5',
+      'Соперник №2',
+    ],
   },
   {
-    title: '5',
-    visibility: 'visible',
+    place: 'Стадион 5',
+    day: '30 сентября',
+    time: '19:00',
+    link: '#',
+    teams: [
+      'Соперник №3',
+      'Соперник №2',
+    ],
   },
 ];
 
 const hexagons = document.querySelectorAll('.events li');
+const teams = document.querySelectorAll('main div span');
+
 hexagons[0].style.visibility = 'hidden';
 hexagons[6].style.visibility = 'hidden';
 
@@ -31,8 +63,15 @@ initial();
 
 function initial() {
   for (let i = 0; i < 5; i++) {
-    hexagons[i + 1].querySelector('.hexagon_text').innerHTML = events[i].title;
+    const hexagon = hexagons[i + 1].querySelector('.hexagon_text').children;
+    hexagon[0].innerHTML = events[i].place;
+    hexagon[1].innerHTML = events[i].day
+    hexagon[2].innerHTML = events[i].time;
+    hexagon[3].href = events[i].link;
+    // hexagon.innerHTML = events[i].place;
   }
+  teams[0].innerHTML = events[eventIndex].teams[0];
+  teams[1].innerHTML = events[eventIndex].teams[1];
 }
 
 function scrollLeft() {
@@ -72,7 +111,7 @@ hexagons.forEach((hexagon, i) => {
       } else {
         scrollLeft();
       }
-    console.log(eventIndex)
+      console.log(eventIndex)
 
     }
   })
@@ -89,6 +128,9 @@ document.addEventListener('wheel', (e) => {
     if (e.deltaY > 30) {
       scrollLeft();
     }
+
+    teams[0].innerHTML = events[eventIndex].teams[0];
+    teams[1].innerHTML = events[eventIndex].teams[1];
 
     setTimeout(() => flag = true, 300)
   }
